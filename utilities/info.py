544d921cb62acf136ed43198932716fa8fb894b1
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2025-04-23 16:52:44 -0300

    Create info.py

---
+++ utilities/info.py
@@ -0,0 +1,3 @@
+def text_TI():
+    '''Functions show what's Tecnology of Information'''
+    print('Tecnologia da Informação (TI) é o conjunto de recursos e técnicas usadas para coletar, armazenar, processar e transmitir informações. Ela abrange desde hardware, como computadores e servidores, até software, redes e segurança digital. A TI é essencial para empresas, governos e indivíduos, ajudando na automação, comunicação e tomada de decisões estratégicas. Com a evolução da inteligência artificial e da computação em nuvem, seu papel se torna cada vez mais crucial no mundo moderno.')
