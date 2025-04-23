dc8db5f55c3f1b9ff3e9f398367680d52ea90f24
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2025-04-23 16:51:03 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from utilities import colors, users, info, menu
+colors.colors()
+
+while True:
+    name = users.get_user_info()
+    #precisa criar uma funcao para pegar a idade do usuario
+    users.welcome(name)
+    users.user_consentiment(name)
+    
+    print()
+    if users.agreement == 'ACEITO':
+        from time import sleep
+        print()
+        info.text_TI()
+        print()
+        sleep(1)
+        print(colors.white +'\nPseudocódigo : 1\nFluxograma : 2'+ colors.reset_color)
+        print()
+        try:
+            option = int(input(colors.white +'Escolha uma opção: '+ colors.reset_color)) 
+        except TypeError:
+            print(colors.red +'valor digitado invalido!'+ colors.reset_color)
+        print()
+        menu.option_menu(option)
+        menu.request_confirmation()
+        sleep(1.5)
+        os.system('clear')
+
+#criar classe usuario
+        
+       
