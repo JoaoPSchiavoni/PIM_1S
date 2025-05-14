b2cbadd448d01e2476a7e125cc5b0c8527011072
Roberto Matheus <robertominiseto6@hotmail.com>
2025-05-14 00:02:18 -0300

    correcao cores menu

--- MacOS/Linux/main.py
+++ MacOS/Linux/main.py
@@ -4,7 +4,6 @@ colors.colors()
 
 while True:
     name = users.get_user_info()
-    #precisa criar uma funcao para pegar a idade do usuario
     users.welcome(name)
     users.user_consentiment(name)
     
