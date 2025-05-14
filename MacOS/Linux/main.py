ffadcf9b939b38b361cb255e8f9e74716440934b
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2025-05-13 21:34:40 -0300

    Teste

--- MacOS/Linux/main.py
+++ MacOS/Linux/main.py
@@ -1,7 +1,9 @@
 
 from utilities import colors, users
 colors.colors()
-
+from time import sleep
+print("hello world")
+sleep(3)
 while True:
     name = users.get_user_info()
     users.welcome(name)
