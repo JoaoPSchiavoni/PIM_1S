fb6bbd9bf4d4e22f67c80eb02750475de1e81500
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2025-05-13 21:48:38 -0300

    Update main.py

--- MacOS/Linux/main.py
+++ MacOS/Linux/main.py
@@ -2,8 +2,7 @@
 from utilities import colors, users
 colors.colors()
 from time import sleep
-print("hello world")
-sleep(3)
+
 while True:
     name = users.get_user_info()
     users.welcome(name)
